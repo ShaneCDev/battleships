51ca17643f5049403620a3b33e0c2d491eae803e
Shane <shaneconway1998@gmail.com>
2022-11-18 23:04:03 +0000

    added create_ships function

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
-import random
+from random import randint
 import os
 import sys
+
+# grid sizes = 3x3, 4x4, 5x5, 7x7
+# X - ship hit
+# '-' - ship miss
+# ~ - ship(s)
 
 letters_to_numbers = {
     'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7
 }
 GUESS_BOARD = [[" "]*8 for i in range(8)]
+COMPUTER_BOARD = [[" "]*8 for i in range(8)]
+GRID_ALPHABET = 'ABCDEFGH'
 
 
 def welcome_message():
@@ -37,5 +44,21 @@ def print_board(board):
     return board
 
 
+def create_ships(comp_board):
+    """
+    Lets the user create ships and creates
+    ships for the computer.
+    """
+    print("Enter the coordinates you would like to place your ships.")
+    print("Separate each coordinate with a comma, eg. A,4")
 
+    for i in range(5):
+        ship_row, ship_column = randint(0, 7), randint(0, 7)
+        while comp_board[ship_row][ship_column] == "~":
+            ship_row, ship_column = randint(0, 7), randint(0, 7)
+        comp_board[ship_row][ship_column] = "~"
+    
+
+create_ships(COMPUTER_BOARD)
+print_board(COMPUTER_BOARD)
 
